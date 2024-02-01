c883bede1a659814c978b81f28d14cfd4f043d3c
ShonRob <rob19063@byui.edu>
2024-01-31 17:33:51 -0700

    implemented the Doc Features

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+    documents: Document[] = [
+      new Document('1', 'Document Name', 'Document Description', 'URL String', null),
+      new Document('2', 'Document Name2', 'Document Description2', 'URL String2', null)
+    ]
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 
 }
