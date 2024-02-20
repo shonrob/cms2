7c59c6a4fdb672155489a401c15bc970832dc0b6
Shonda Robinson <64099427+shonrob@users.noreply.github.com>
2024-02-19 21:29:41 -0700

    view and edit buttons working in documents

--- src/app/documents/document-detail/document-detail.component.ts
+++ src/app/documents/document-detail/document-detail.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 // COMPONENTS IMPORTS
 import { Document } from '../document.model';
 import { DocumentService } from '../document.service';
+import { WindRefService } from '../../wind-ref.service';
 
 
 @Component({
@@ -15,9 +16,11 @@ import { DocumentService } from '../document.service';
 export class DocumentDetailComponent {
   document: Document;
   id: string;
+  nativeWindow: any;
 
   // CONSTRUCTOR 
   constructor(private documentService: DocumentService,
+              private windowRefService: WindRefService,
               private route: ActivatedRoute,
               private router: Router, 
               ){}
@@ -27,7 +30,15 @@ export class DocumentDetailComponent {
       this.id = params.id;
       this.document = this.documentService.getaDocument(this.id);
     });
-
   }
+
+onView() {
+  this.nativeWindow = this.windowRefService.getNativeWindow();
+  if (this.document.url) {
+    this.nativeWindow.open(this.document.url);
+  }
+}
+
+
   
 } 
