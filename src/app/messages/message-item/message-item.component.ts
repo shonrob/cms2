dd72250ff29abc1a416875d3139ed8e1662e818f
Shonda Robinson <64099427+shonrob@users.noreply.github.com>
2024-03-16 15:54:50 -0600

    MessageDelete button is working! YAY

--- src/app/messages/message-item/message-item.component.ts
+++ src/app/messages/message-item/message-item.component.ts
@@ -2,28 +2,36 @@ import { Component, Input } from '@angular/core';
 import { Message } from '../message.model';
 import { ContactService } from '../../contacts/contact.service';
 import { Contact } from '../../contacts/contact.model';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'cms-message-item',
   templateUrl: './message-item.component.html',
-  styleUrl: './message-item.component.css'
+  styleUrl: './message-item.component.css',
 })
 export class MessageItemComponent {
   @Input() message: Message;
 
-  // VARIABLES 
+  // VARIABLES;
   messageSender: string;
+  id: string;
 
-  // CONSTRUCTOR 
-  constructor(private contactService: ContactService) {}
+  // CONSTRUCTOR
+  constructor(
+    private contactService: ContactService,
+    private messageService: MessageService
+  ) {}
 
-// METHODS 
+  // METHODS
   ngOnInit(): void {
-    const contact: Contact = this.contactService.getaContact(this.message.sender);
+    const contact: Contact = this.contactService.getaContact(
+      this.message.sender
+    );
     this.messageSender = contact.name;
   }
 
-  
-
+  deleteMessage() {
+    this.messageService.deleteMessage(this.message);
+    console.log('hello');
+  }
 }
- 
