77ac5c4a88fd11aedea20be39a62cb5f2438c237
Shonda Robinson <64099427+shonrob@users.noreply.github.com>
2024-03-16 12:28:38 -0600

    chaos

---
+++ server/routes/app.js
@@ -0,0 +1,8 @@
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "dist/cms2/browser/index.html"));
+});
+
+module.exports = router;
